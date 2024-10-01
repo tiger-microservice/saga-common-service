bb7d2bc78c721cb9aaf68227ab3af1a99ade18a7
Ta Duy Hoang <duyhoangptit@gmail.com>
2024-10-01 15:13:41 +0700

    implement saga pattern with axon

---
+++ src/main/java/vn/tiger/sagacommon/constants/enums/PaymentStatus.java
@@ -0,0 +1,10 @@
+package vn.tiger.sagacommon.constants.enums;
+
+import lombok.ToString;
+
+@ToString
+public enum PaymentStatus {
+    CREATED,
+    APPROVED,
+    CANCELLED
+}
---
+++ src/main/java/vn/tiger/sagacommon/constants/enums/ShippingStatus.java
@@ -0,0 +1,10 @@
+package vn.tiger.sagacommon.constants.enums;
+
+import lombok.ToString;
+
+@ToString
+public enum ShippingStatus {
+    CREATED,
+    APPROVED,
+    CANCELLED
+}
